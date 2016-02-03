40ac5a75924bff7901a928ace88ccf99ef3f790f
BlueNexus <benreeve99@yahoo.co.uk>
2016-02-03 14:18:07 +0000

    Even more error checking

--- PyPass/Core.py
+++ PyPass/Core.py
@@ -26,14 +26,22 @@ While True:
     if config.USE_SECURE_SEED is True:
         seed = os.urandom(config.LEN_SECURE_SEED)
         random.seed(seed)
-    length = int(raw_input("How long should the password be?: "))
-    count = int(raw_input("How many users would you like to generate passwords for?: "))
+    try:
+        length = int(raw_input("How long should the password be?: "))
+        count = int(raw_input("How many users would you like to generate passwords for?: "))
+    except:
+        print "ERROR: INVALID INPUT"
+        continue
     for password in range(1, count+1):
         pword = generate(valList, length)
         current = {password: pword}
         user.append(current)
 
-    with open(str(config.OUTPUT_FILE), "w") as res:
-        for item in user:
-            for field, field2 in item.iteritems():
-                res.write(str(field) + ", " + str(field2) + "\n")
+    try:
+        with open(str(config.OUTPUT_FILE), "w") as res:
+            for item in user:
+                for field, field2 in item.iteritems():
+                    res.write(str(field) + ", " + str(field2) + "\n")
+    except:
+        print "ERROR: FILE ERROR"
+        continue
