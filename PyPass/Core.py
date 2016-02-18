0e7b95d75cb9312574999a94747fc99eb2b1f586
BlueNexus <benreeve99@yahoo.co.uk>
2016-02-18 13:19:36 +0000

    Merging multi and single into one file

--- PyPass/Core.py
+++ PyPass/Core.py
@@ -28,20 +28,26 @@ While True:
         random.seed(seed)
     try:
         length = int(raw_input("How long should the password be?: "))
-        count = int(raw_input("How many users would you like to generate passwords for?: "))
+        if config.MODE == 2:
+            count = int(raw_input("How many users would you like to generate passwords for?: "))
     except:
         print "ERROR: INVALID INPUT"
         continue
-    for password in range(1, count+1):
+    if config.MODE == 1:
         pword = generate(valList, length)
         current = {password: pword}
-        user.append(current)
+        print current
+    else:
+        for password in range(1, count+1):
+            pword = generate(valList, length)
+            current = {password: pword}
+            user.append(current)
 
-    try:
-        with open(str(config.OUTPUT_FILE), "w") as res:
-            for item in user:
-                for field, field2 in item.iteritems():
-                    res.write(str(field) + ", " + str(field2) + "\n")
-    except:
-        print "ERROR: FILE ERROR"
-        continue
+        try:
+            with open(str(config.OUTPUT_FILE), "w") as res:
+                for item in user:
+                    for field, field2 in item.iteritems():
+                        res.write(str(field) + ", " + str(field2) + "\n")
+        except:
+            print "ERROR: FILE ERROR"
+            continue
