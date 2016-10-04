75355287350cdad41b39c13eae250c0d31654b56
BlueNexus <benreeve99@yahoo.co.uk>
2016-10-04 20:45:41 +0100

    Final bugfixes

--- PyPass/config.py
+++ PyPass/config.py
@@ -21,11 +21,11 @@
 # 2: Multi
 # # # # # # # # # # #
 
-SEC_LEVEL = 1
-USE_SECURE_SEED = True
-LEN_SECURE_SEED = 8
-OUTPUT_FILE = "output.csv"
-MODE = 2
+C_SEC_LEVEL = 1
+C_USE_SECURE_SEED = True
+C_LEN_SECURE_SEED = 8
+C_OUTPUT_FILE = "results.csv"
+C_MODE = 1
 
 # # # # # # # # # # # # # # # #
 # DO NOT EDIT BELOW THIS LINE
@@ -33,48 +33,48 @@ MODE = 2
 
 
 def SEC_LEVEL():
-    if SEC_LEVEL is not None and 0 < SEC_LEVEL > 4:
-        return(SEC_LEVEL)
+    if C_SEC_LEVEL is not None and 0 < C_SEC_LEVEL < 4:
+        return(C_SEC_LEVEL)
     else:
-        if SEC_LEVEL is None:
-            print("ERROR: NULL VALUE(SEC_LEVEL)")
+        if C_SEC_LEVEL is None:
+            print("CONFIG_ERROR: NULL VALUE(SEC_LEVEL)")
         else:
-            print("ERROR: INVALID VALUE(SEC_LEVEL")
+            print("CONFIG_ERROR: INVALID VALUE(SEC_LEVEL)")
         exit()
 
 
 def USE_SECURE_SEED():
-    if USE_SECURE_SEED is not None and USE_SECURE_SEED in (True, False):
-        return (USE_SECURE_SEED)
+    if C_USE_SECURE_SEED is not None and C_USE_SECURE_SEED in (True, False):
+        return (C_USE_SECURE_SEED)
     else:
-        if USE_SECURE_SEED is None:
-            print("ERROR: NULL VALUE(USE_SECURE_SEED)")
+        if C_USE_SECURE_SEED is None:
+            print("CONFIG_ERROR: NULL VALUE(USE_SECURE_SEED)")
         else:
-            print("ERROR: INVALID VALUE(USE_SECURE_SEED)")
+            print("CONFIG_ERROR: INVALID VALUE(USE_SECURE_SEED)")
         exit()
 
 
 def LEN_SECURE_SEED():
-    if LEN_SECURE_SEED is not None and 0 < LEN_SECURE_SEED:
-        return(LEN_SECURE_SEED)
+    if C_LEN_SECURE_SEED is not None and 0 < C_LEN_SECURE_SEED:
+        return(C_LEN_SECURE_SEED)
     else:
-        if LEN_SECURE_SEED is None:
-            print("ERROR: NULL VALUE(LEN_SECURE_SEED)")
+        if C_LEN_SECURE_SEED is None:
+            print("CONFIG_ERROR: NULL VALUE(LEN_SECURE_SEED)")
         else:
-            print("ERROR: INVALID VALUE(LEN_SECURE_SEED)")
+            print("CONFIG_ERROR: INVALID VALUE(LEN_SECURE_SEED)")
         exit()
 
 
 def OUTPUT_FILE():
-    if OUTPUT_FILE is not None:
-        return(OUTPUT_FILE)
+    if C_OUTPUT_FILE is not None:
+        return(C_OUTPUT_FILE)
     else:
-        print("ERROR: NULL VALUE(OUTPUT_FILE)")
+        print("CONFIG_ERROR: NULL VALUE(OUTPUT_FILE)")
         exit()
 
 def MODE():
-    if MODE in (1, 2):
-        return(MODE)
+    if C_MODE in (1, 2):
+        return(C_MODE)
     else:
-        print("ERROR: INVALID VALUE(MODE)")
+        print("CONFIG_ERROR: INVALID VALUE(MODE)")
         exit()
