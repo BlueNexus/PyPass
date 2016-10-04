6153bd8e602748d1430f35c7f1f6e02b0acef1e0
BlueNexus <benreeve99@yahoo.co.uk>
2016-10-04 20:44:28 +0100

    Final bugfixes

--- PyPass/core.py
+++ PyPass/core.py
@@ -8,11 +8,11 @@ import config
 
 user = []
 
-if config.SEC_LEVEL == 1:
+if config.SEC_LEVEL() == 1:
     valList = list(string.ascii_letters + string.digits)
-elif config.SEC_LEVEL == 2:
+elif config.SEC_LEVEL() == 2:
     valList = list(string.ascii_letters + string.digits + string.punctuation)
-elif config.SEC_LEVEL == 3:
+elif config.SEC_LEVEL() == 3:
     valList = list(string.printable)
 else:
     print("ERROR: INVALID VALUE(SEC_LEVEL)")
@@ -22,23 +22,23 @@ else:
 def generate(valList, length):
     password = []
     for char in range(1, length + 1):
-        password.append(str(valList[random.randint(1, len(valList))]))
+        password.append(str(valList[random.randint(1, len(valList) - 1)]))
     return("".join(password))
 
-While True:
-    if config.USE_SECURE_SEED is True:
-        seed = os.urandom(config.LEN_SECURE_SEED)
+while True:
+    if config.USE_SECURE_SEED() is True:
+        seed = os.urandom(config.LEN_SECURE_SEED())
         random.seed(seed)
     try:
         length = int(input("How long should the password be?: "))
-        if config.MODE == 2:
+        if config.MODE() == 2:
             count = int(input("How many users would you like to generate passwords for?: "))
     except:
-        print "ERROR: INVALID INPUT"
+        print("ERROR: INVALID INPUT")
         continue
-    if config.MODE == 1:
+    if config.MODE() == 1:
         pword = generate(valList, length)
-        current = {password: pword}
+        current = {pword}
         print(current)
     else:
         for password in range(1, count+1):
@@ -47,10 +47,11 @@ While True:
             user.append(current)
 
         try:
-            with open(str(config.OUTPUT_FILE), "w") as res:
+            with open(str(config.OUTPUT_FILE()), "w") as res:
                 for item in user:
-                    for field, field2 in item.iteritems():
+                    for field, field2 in item.items():
                         res.write(str(field) + ", " + str(field2) + "\n")
+                print("Done.")
         except:
             print("ERROR: FILE ERROR")
             continue
