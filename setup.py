64a8478fb86e34a73d0a0402962e97cd5e51a366
BlueNexus <benreeve99@yahoo.co.uk>
2017-04-24 13:14:00 +0100

    Adds a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PyPass',
+      version='3.0.0',
+      description='Simple password generator',
+      url='https://github.com/BlueNexus/PyPass',
+      author='Ben Reeve',
+      author_email='benreeve99@yahoo.co.uk',
+      install_requires=['appJar'],
+      dependency_links=['https://github.com/RWBA/appJar/blob/appJar/releases/appJar.zip?raw=true']
+      )
