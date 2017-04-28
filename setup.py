a2ad79567039271e83bc510a6c944d7747b5d511
BlueNexus <benreeve99@yahoo.co.uk>
2017-04-28 12:02:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PyPass',
+	version='3.0',
+	author='Ben J. C. Reeve',
+	author_email='benreeve99@yahoo.co.uk',
+	packages=['PyPass'],
+	install_requires=['appJar'],
+	dependency_links = ['https://github.com/jarvisteach/appJar']
+)
